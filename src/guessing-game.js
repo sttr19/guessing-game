fdfbfb01f4af35c2867966eebbe795b422be16e0
sttr19 <stastroshko@gmail.com>
2022-08-16 16:57:18 +0300

    feat:task solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return Math.round((this.maxValue + this.minValue) / 2);
     }
 
     lower() {
-
+        return this.maxValue = (Math.round((this.maxValue + this.minValue) / 2));
     }
 
     greater() {
-
+        return this.minValue = (Math.round((this.maxValue + this.minValue) / 2));
     }
 }
 
